9551e3a7d283b548411979a0415b5672bed02b30
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2022-06-23 20:43:21 +0900

    [FEA] Spring Container 생성
     - @Configuration, ApplicationContext, AnnotationConfigApplicationContext
     - @Bean 이해

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -6,13 +6,19 @@ import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
         Long memberId = 1L;
         Member member1 = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member1);
