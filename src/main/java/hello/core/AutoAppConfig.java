ad977200e554eeddeae616bbba1a139ad0aceb3c
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2022-06-26 13:43:20 +0900

    [FEA] ComponentScan  - 컴프넌트 스캔 의존관계 및 기본값 처리  - 필터, 중복등록시 처리  49

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
+                classes = Configuration.class)
+)
+public class AutoAppConfig {
+    // 수동 빈이 우선순위로 오버라이딩 해준다.
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
