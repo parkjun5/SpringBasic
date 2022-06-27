b28b5e22dfcf8d94cb9b2adc594230e09cc27f39
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2022-06-27 23:13:26 +0900

    [FEA] 의존관계 자동 주입 어노테이션 컨트롤 예제
     - 어노테이션 생성 및 빈 컨트롤 방식
     - 자식(implements)들을 한번에 테스트 하는 방식 주입받아 map or list for문으로 컨트롤
     - 빈 생명주기 콜백 생성시, 제거시 처리
    
     66

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,52 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice1 = discountService.discount(member, 5000, "fixDiscountPolicy");
+        assertThat(discountPrice1).isEqualTo(1000);
+
+        int discountPrice2 = discountService.discount(member, 30000, "rateDiscountPolicy");
+        assertThat(discountPrice2).isEqualTo(3000);
+
+
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> polices;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> polices) {
+            this.policyMap = policyMap;
+            this.polices = polices;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("polices = " + polices);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            return policyMap.get(discountCode).discount(member, price);
+        }
+    }
+
+}
